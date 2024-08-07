35fdb7d8557f5a68b42244eaa7c907bae4d63ba1
lilonghui <2276110742@qq.com>
2024-08-07 12:48:38 +0800

    添加模型识别

--- mcd/video.py
+++ mcd/video.py
@@ -1,4 +1,16 @@
 import cv2
+from ultralytics import YOLO
+model = YOLO('yolov8n.pt')  # 使用 YOLOv8n 预训练模型
+
+
+def get_available_cameras(max_cameras=10):
+    available_cameras = []
+    for camera_id in range(max_cameras):
+        cap = cv2.VideoCapture(camera_id)
+        if cap.isOpened():
+            available_cameras.append(camera_id)
+            cap.release()  # 释放摄像头
+    return available_cameras
 
 def analyze_frames(source=0):
     cap = cv2.VideoCapture(source)  # 捕获摄像头输入，0 表示默认摄像头
@@ -7,11 +19,12 @@ def analyze_frames(source=0):
         if not ret:
             break
         
-        # 处理帧的地方，示例中我们将帧转换为灰度
-        frame = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+        # 进行目标检测
+        results = model(frame)
+        annotated_frame = results[0].plot()  # 在帧上绘制检测结果
         
         # 编码帧为JPEG格式
-        ret, buffer = cv2.imencode('.jpg', frame)
+        ret, buffer = cv2.imencode('.jpg', annotated_frame)
         yield buffer.tobytes()
 
     cap.release()
--- web/app.py
+++ web/app.py
@@ -1,69 +1,41 @@
-from fastapi import FastAPI, Depends, Request, Response
+from fastapi import FastAPI, Request, Response
 from fastapi.responses import JSONResponse,StreamingResponse
-from mcd.video import capture_frames,gray_frames,analyze_frames
-import uvicorn
-import uuid
+import mcd.video as v
 
 app = FastAPI()
 
-SESSION_COOKIE_NAME = "session_id"
-
-# Simulate a session store (in-memory for this example)
-session_store = {}
-
-
-def get_or_create_session_id(request: Request, response: Response) -> str:
-    session_id = request.cookies.get(SESSION_COOKIE_NAME)
-    if not session_id or session_id not in session_store:
-        # Generate a new session ID and create a session
-        session_id = str(uuid.uuid4())  # Create a unique session ID
-        session_store[session_id] = {"user": "Anonymous User"}
-        response.set_cookie(key=SESSION_COOKIE_NAME, value=session_id)
-    return session_id
-
-@app.get("/profile")
-async def profile(session_id: str = Depends(get_or_create_session_id)):
-    user_data = session_store[session_id]
-    return {"profile": user_data}
-
-@app.post("/logout")
-async def logout(response: Response, session_id: str = Depends(get_or_create_session_id)):
-    if session_id and session_id in session_store:
-        del session_store[session_id]
-        response.delete_cookie(SESSION_COOKIE_NAME)
-        return {"message": "Logged out"}
-    else:
-        return JSONResponse(status_code=401, content={"message": "Unauthorized"})
-
+# 存储全局信息
+global_store = {
+    "capture_addr": 0
+}
 
 @app.get("/capture_addr")
-async def get_capture_addr(request: Request, response: Response):
-    session_id = get_or_create_session_id(request,response)
-    return session_store[session_id]['capture_addr']
+async def get_capture_addr():
+    return global_store['capture_addr']
 
+
+@app.get("/available_cameras")
+async def get_available_cameras():
+    return v.get_available_cameras()
 
 @app.post("/capture_addr")
 async def put_capture_addr(request: Request, response: Response):
-    session_id = get_or_create_session_id(request,response)
-    session_store[session_id]['capture_addr'] = request.json()['capture_addr']
-    return {"profile": session_store[session_id]}
+    global_store['capture_addr'] = request.json()['capture_addr']
+    return {"capture_addr": global_store['capture_addr']}
 
 def pad_frames(frames_generator):
     for frame in frames_generator:
         yield (b'--frame\r\n Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n')
-        
 
 @app.get('/video_source_feed')
 async def video_source_feed(request: Request, response: Response):
-    session_id = get_or_create_session_id(request,response)
-    source = session_store[session_id]['capture_addr'] or 0
-    return StreamingResponse(pad_frames(capture_frames(source)), media_type='multipart/x-mixed-replace; boundary=frame')
+    source = global_store['capture_addr']
+    return StreamingResponse(pad_frames(v.capture_frames(source)), media_type='multipart/x-mixed-replace; boundary=frame')
 
 @app.get('/video_output_feed')
 async def video_output_feed(request: Request, response: Response):
-    session_id = get_or_create_session_id(request,response)
-    source = session_store[session_id]['capture_addr'] or 0
-    return StreamingResponse(pad_frames(gray_frames(source)), media_type='multipart/x-mixed-replace; boundary=frame')
+    source = global_store['capture_addr']
+    return StreamingResponse(pad_frames(v.analyze_frames(source)), media_type='multipart/x-mixed-replace; boundary=frame')
 
 
 if __name__ == "__main__":
