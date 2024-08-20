4ddbe15b01eb309079f9dca7b806a8ef3d75e66a
lilonghui <2276110742@qq.com>
2024-08-20 23:19:30 +0800

    video_model_output_file bug

--- api/app.py
+++ api/app.py
@@ -98,17 +98,17 @@ async def get_config():
         configSetting.camera_local = conf.huiji_detect_config["camera_source"]
         configSetting.camera_url = ""
         configSetting.data_type = conf.huiji_detect_config['data_source_type']
-        configSetting.data_file_source = conf.huiji_detect_config['video_file']
-        configSetting.data_file_target  =conf.huiji_detect_config['video_model_output_file']
+        configSetting.data_file_source = conf.huiji_detect_config.get('video_file')
+        configSetting.data_file_target  =conf.huiji_detect_config.get('video_model_output_file')
         
 
     if conf.current_mode == 'person_detect':
         configSetting.camera_type = 0
         configSetting.camera_local = conf.person_detect_config["camera_source"]
         configSetting.camera_url = ""
-        configSetting.data_type = conf.person_detect_config['data_source_type']
-        configSetting.data_file_source = conf.person_detect_config['video_file']
-        configSetting.data_file_target  =conf.person_detect_config['video_model_output_file']
+        configSetting.data_type = conf.person_detect_config.get('data_source_type')
+        configSetting.data_file_source = conf.person_detect_config.get('video_file')
+        configSetting.data_file_target  =conf.person_detect_config.get('video_model_output_file')
 
     return {
         "code":0,
