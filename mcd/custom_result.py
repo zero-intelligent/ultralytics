10d600fb6983a38f00a8e7ea81f460b54ac93fc3
lilonghui <2276110742@qq.com>
2024-08-22 11:18:42 +0800

    format custom result

--- mcd/custom_result.py
+++ mcd/custom_result.py
@@ -1,49 +1,40 @@
 from ultralytics.engine.results import Results
 from copy import deepcopy
-from functools import lru_cache
-from pathlib import Path
-
-import numpy as np
 import torch
+from ultralytics.utils.plotting import Annotator
 
-from ultralytics.data.augment import LetterBox
-from ultralytics.utils import LOGGER, SimpleClass, ops
-from ultralytics.utils.plotting import Annotator, colors, save_one_box
-from ultralytics.utils.torch_utils import smart_inference_mode
 
 class PersonResults(Results):
     id_info = {}
 
-    # 覆盖基类的方法，否则复制对象时，类型信息又回去了
-
-    def __init__(
-        self, **kwargs
-    ) -> None:
+    def __init__(self, **kwargs):
         super().__init__(**kwargs)
 
+    # 覆盖基类的方法，否则复制对象时，类型信息又回去了
     def new(self):
         return PersonResults(orig_img=self.orig_img, path=self.path, names=self.names, speed=self.speed)
-    
+
     def plot(self,
-            conf=True,
-            line_width=None,
-            font_size=None,
-            font="Arial.ttf",
-            pil=False,
-            img=None,
-            im_gpu=None,
-            kpt_radius=5,
-            kpt_line=True,
-            labels=True,
-            boxes=True,
-            masks=True,
-            probs=True,
-            show=False,
-            save=False,
-            filename=None
-        ):
+             conf=True,
+             line_width=None,
+             font_size=None,
+             font="Arial.ttf",
+             pil=False,
+             img=None,
+             im_gpu=None,
+             kpt_radius=5,
+             kpt_line=True,
+             labels=True,
+             boxes=True,
+             masks=True,
+             probs=True,
+             show=False,
+             save=False,
+             filename=None
+             ):
         if img is None and isinstance(self.orig_img, torch.Tensor):
-            img = (self.orig_img[0].detach().permute(1, 2, 0).contiguous() * 255).to(torch.uint8).cpu().numpy()
+            img = (self.orig_img[0].detach().permute(
+                1, 2, 0).contiguous() * 255).to(torch.uint8).cpu().numpy()
 
         names = self.names
         is_obb = self.obb is not None
@@ -55,14 +46,16 @@ class PersonResults(Results):
             line_width,
             font_size,
             font,
-            pil or (pred_probs is not None and show_probs),  # Classify tasks default to pil=True
+            # Classify tasks default to pil=True
+            pil or (pred_probs is not None and show_probs),
             example=names,
         )
 
         # Plot Detect results
         if pred_boxes is not None and show_boxes:
             for d in reversed(pred_boxes):
-                c, conf, id = int(d.cls), float(d.conf) if conf else None, None if d.id is None else int(d.id.item())
+                c, conf, id = int(d.cls), float(
+                    d.conf) if conf else None, None if d.id is None else int(d.id.item())
                 if not id:
                     continue
                 if id not in PersonResults.id_info:
@@ -72,25 +65,33 @@ class PersonResults(Results):
                                   int((float(d.xyxy[0][1]) + float(d.xyxy[0][3]))/2)]]
                     }
                 else:
-                    PersonResults.id_info[id]['time_s'] += 1.0/20  # 每发现一帧，积累每帧的时间
+                    # 每发现一帧，积累每帧的时间
+                    PersonResults.id_info[id]['time_s'] += 1.0/20
                     PersonResults.id_info[id]['path'] += [[int((float(d.xyxy[0][0]) + float(d.xyxy[0][2]))/2),
-                                                     int((float(d.xyxy[0][1]) + float(d.xyxy[0][3]))/2)]] # 添加路径坐标
-                    
+                                                           # 添加路径坐标
+                                                           int((float(d.xyxy[0][1]) + float(d.xyxy[0][3]))/2)]]
+
                 label = f"id:{id},{int(PersonResults.id_info[id]['time_s'])}s"
-                xy = [int((float(d.xyxy[0][0]) + float(d.xyxy[0][2]))/2)-8,int(d.xyxy[0][1])]
-                annotator.text(xy, label, txt_color=(48,128,20),box_style=True)
-                annotator.routing_path(PersonResults.id_info[id]['path'],color=(48,128,20))
+                xy = [
+                    int((float(d.xyxy[0][0]) + float(d.xyxy[0][2]))/2)-8, int(d.xyxy[0][1])]
+                annotator.text(xy, label, txt_color=(
+                    48, 128, 20), box_style=True)
+                annotator.routing_path(
+                    PersonResults.id_info[id]['path'], color=(48, 128, 20))
 
         # Plot Classify results
         if pred_probs is not None and show_probs:
-            text = ",\n".join(f"{names[j] if names else j} {pred_probs.data[j]:.2f}" for j in pred_probs.top5)
+            text = ",\n".join(f"{names[j] if names else j} {
+                              pred_probs.data[j]:.2f}" for j in pred_probs.top5)
             x = round(self.orig_shape[0] * 0.03)
-            annotator.text([x, x], text, txt_color=(255, 255, 255))  # TODO: allow setting colors
+            # TODO: allow setting colors
+            annotator.text([x, x], text, txt_color=(255, 255, 255))
 
         # Plot Pose results
         if self.keypoints is not None:
             for k in reversed(self.keypoints.data):
-                annotator.kpts(k, self.orig_shape, radius=kpt_radius, kpt_line=kpt_line)
+                annotator.kpts(k, self.orig_shape,
+                               radius=kpt_radius, kpt_line=kpt_line)
 
         # Show results
         if show:
@@ -101,4 +102,3 @@ class PersonResults(Results):
             annotator.save(filename)
 
         return annotator.result()
-
