50e37c22c92c1a31ff04cb950c945591deb9e117
lilonghui <2276110742@qq.com>
2024-08-31 20:23:42 +0800

    dependency pip install

--- build.sh
+++ build.sh
@@ -4,5 +4,7 @@
 # npm run build
 
 # cd ../
-pip install build -i https://mirrors.aliyun.com/pypi/simple
+if ! pip install -i https://mirrors.aliyun.com/pypi/simple build setuptools wheel;then
+    echo "install module build setuptools wheel fail!"
+    exit 1
 python -m build
