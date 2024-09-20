d483c3004eea6585ae4426486ecb9c3fbad3ff1e
lilonghui <2276110742@qq.com>
2024-09-20 23:42:24 +0800

    dir

--- mcd/scripts/train_pub.sh
+++ mcd/scripts/train_pub.sh
@@ -12,17 +12,11 @@ ssh -p 2222 admin@8.140.49.13 'cd python_projects/ultralytics;conda activate ult
 
 echo "训练结果模型复制到本地"
 dir=$(ssh -p 2222 admin@8.140.49.13 'ls /home/admin/python_projects/ultralytics/runs/detect/* -td | head -1')
-scp -P 2222  admin@8.140.49.13:$(dir)/weights/best.onnx ~/python_projects/ultralytics/mcd/weights/huiji.onnx
+scp -P 2222  admin@8.140.49.13:${dir}/weights/best.onnx ~/python_projects/ultralytics/mcd/weights/huiji.onnx
 
 echo "本地训练模型复制到鲁班猫"
 scp -P 2224 ~/python_projects/ultralytics/mcd/weights/huiji.onnx cat@8.140.49.13:/home/cat/app/ultralytics/mcd/weights/huiji.onnx
 
 echo "重启鲁班猫服务"
 ssh -p 2224 admin@8.140.49.13 'sudo systemctl restart mcd-video-analysis.service'
-
-
-
-
-
-
 
