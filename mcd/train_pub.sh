6f7c2e94af698840412a87b9cdae5d68ca5b31bf
lilonghui <2276110742@qq.com>
2024-09-20 21:53:06 +0800

    训练和发布脚本

---
+++ mcd/train_pub.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+echo "备份服务器训练数据"
+ssh -p 2222 admin@8.140.49.13 'cd python_projects/ultralytics/datasets;mv MCD MCD_$(date +"%Y%m%d%H")'
+
+echo "复制训练数据到服务器"
+scp -P 2222 -r ~/Downloads/MCD admin@8.140.49.13:/home/admin/python_projects/ultralytics/datasets/
+
+echo "开始训练模型"
+ssh -p 2222 admin@8.140.49.13 'cd python_projects/ultralytics;conda activate ultralytics;python mcd/train.py datasets/MCD'
+
+echo "训练结果模型复制到本地"
+scp -P 2222  admin@8.140.49.13:/home/admin/python_projects/ultralytics/runs/detect/$(date +"%Y-%m-%d.%H")/weights/best.onnx ~/python_projects/ultralytics/mcd/weights/huiji.onnx
+
+echo "本地训练模型复制到鲁班猫"
+scp -P 2224 ~/python_projects/ultralytics/mcd/weights/huiji.onnx cat@8.140.49.13:/home/cat/app/ultralytics/mcd/weights/huiji.onnx
+
+echo "重启鲁班猫服务"
+ssh -p 2224 admin@8.140.49.13 'sudo systemctl restart mcd-video-analysis.service'
+
+
+
+
+
+
+
