028784154e55797773f2b886d64927ea6b3f1314
lilonghui <2276110742@qq.com>
2024-09-20 15:20:15 +0800

    mcd/weights/

--- mcd/conf.py
+++ mcd/conf.py
@@ -24,7 +24,7 @@ huiji_detect_config = {
     "current_taocan_id": 0,
 
     #当前使用的模型
-    "model": "mcd/huiji_detect_model.onnx",
+    "model": "mcd/weights/huiji_detect_model.onnx",
 
     # 食品清单信息：id,EnglishName, ChineseName
     "meals_info": [
@@ -78,7 +78,7 @@ person_detect_config = {
     # 当前摄像头地址，本地为 0,1,2， 网络是 uri 地址
     "camera_source": 0,
     "video_file": "",
-    "model": "yolov8n.pt"
+    "model": "mcd/weights/yolov8n.onnx"
 
 }
 
