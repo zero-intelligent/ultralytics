f04d3ce22dd578513390a0ec0903ec44c1ddf1b9
lilonghui <2276110742@qq.com>
2024-09-21 18:53:29 +0800

    classmethod

--- mcd/session.py
+++ mcd/session.py
@@ -8,20 +8,20 @@ class UserMonitor:
     #最后一个用户的离开时间
     ws_last_disconnect_time = None
 
-    @staticmethod
-    def user_enter():
-        UserMonitor.ws_connection_cnt += 1
+    @classmethod
+    def user_enter(cls):
+        cls.ws_connection_cnt += 1
     
-    @staticmethod
-    def user_leave():
-        UserMonitor.ws_connection_cnt -= 1
+    @classmethod
+    def user_leave(cls):
+        cls.ws_connection_cnt -= 1
         
-        UserMonitor.ws_last_disconnect_time = time.time()  # 记录离开时间
+        cls.ws_last_disconnect_time = time.time()  # 记录离开时间
     
-    @staticmethod
-    def has_active_client(timeout:int = 60):
-        return UserMonitor.ws_connection_cnt > 0 \
-            or UserMonitor.ws_last_disconnect_time is not None \
-            and UserMonitor.ws_last_disconnect_time + timeout > time.time()
+    @classmethod
+    def has_active_client(cls,timeout:int = 60):
+        return cls.ws_connection_cnt > 0 \
+            or cls.ws_last_disconnect_time is not None \
+            and cls.ws_last_disconnect_time + timeout > time.time()
 
 
